3809753b9d38713d2bfa1e4753977e69707d358f
abcdefg-max <sarathamuruganandham@gmail.com>
2020-05-03 19:03:14 +0530

    making slingshot

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform1;
-
+var backgroundImg,platform;
+var bird, slingShot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,7 +19,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform1 = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -37,14 +38,14 @@ function setup(){
 
     bird = new Bird(100,100);
 
+   // log6 = new Log(230,180,80, PI/2);
+    slingshot= new Slingshot(bird.body,{ x:200,y :100});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -62,4 +63,15 @@ function draw(){
 
     bird.display();
     platform.display();
+    //log6.display();
+    slingshot.display();    
 }
+
+function mouseDragged() {
+    Matter.Body.setPosition(bird.body,{x: mouseX,y: mouseY});
+}
+function mouseReleased(){
+    slingshot.fly();
+
+}
+    
